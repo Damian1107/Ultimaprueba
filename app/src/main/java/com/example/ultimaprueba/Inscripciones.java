7099dc91e6cdefc7808b6dd284bd81f632361d75
Damian1107 <demianx1993@gmail.com>
2019-11-28 22:49:05 -0300

    Ultima Prueba

---
+++ app/src/main/java/com/example/ultimaprueba/Inscripciones.java
@@ -0,0 +1,38 @@
+package com.example.ultimaprueba;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLDataException;
+
+public class Inscripciones extends SQLiteOpenHelper {
+
+    private static final String NOMBRE_BD = "Inscripciones.bd";
+    private static final int VERSION_BD = 1;
+    private static final String TABLA_CURSOS = "CREATE TABLE CURSOS(USUARIOS TEXT PRIMARY KEY, RAMOS TEXT, PROFESION TEXT)";
+
+    public Inscripciones(Context context) {
+        super(context, NOMBRE_BD, null, VERSION_BD);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase){
+        sqLiteDatabase.execSQL(TABLA_CURSOS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLA_CURSOS);
+        sqLiteDatabase.execSQL(TABLA_CURSOS);
+    }
+
+    public void agregarCursos(String usuarios, String ramos, String profesion){
+        SQLiteDatabase bd = getWritableDatabase();
+        if(bd!=null){
+            bd.execSQL("INSERT INTO RAMOS VALUES ('"+usuarios+"','"+ramos+"','"+profesion+"')");
+            bd.close();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/ultimaprueba/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ultimaprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editUsuarios, editRamos, editProfesion;
+    Button buttonAceptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editUsuarios=(EditText)findViewById(R.id.editUsuarios);
+        editRamos=(EditText)findViewById(R.id.editRamos);
+        editProfesion=(EditText)findViewById(R.id.editProfesion);
+        buttonAceptar=(Button) findViewById(R.id.buttonAceptar);
+
+        final Inscripciones Inscripciones_bd = new Inscripciones(getApplicationContext());
+
+        buttonAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Inscripciones_bd.agregarCursos(editUsuarios.getText().toString(),editRamos.getText().toString(),editProfesion.getText().toString());
+                Toast.makeText(getApplicationContext(), "SE AGREGO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
